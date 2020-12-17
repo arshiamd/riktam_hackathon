edb868132fc9400b4dd0499887f60d82bc9bdede
arshia <arshiamd.777@gmail.com>
2020-12-17 20:11:09 +0530

    views

--- covidpledge/views.py
+++ covidpledge/views.py
@@ -8,19 +8,6 @@ def loginPage(request):
     return render(request,'login.html')
 
 def registerPage(request):
-    # if request.user.is_authenticated:
-	#     return redirect('home')
-    # else:
-    #     form = UserCreationForm()
-    #     if request.method == 'POST':
-	# 		form = CreateUserForm(request.POST)
-	# 		if form.is_valid():
-	# 			form.save()
-	# 			user = form.cleaned_data.get('username')
-	# 			messages.success(request, 'Account was created for ' + user)
-
-	# 			return redirect('login')
-
     return render(request,'register.html')
 
 def contact(request):
